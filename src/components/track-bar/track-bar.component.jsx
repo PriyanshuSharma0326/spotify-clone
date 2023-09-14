4447c44bb2156acda542688a3f9822a9714ca0bc
PriyanshuSharma0326 <ps940016@gmail.com>
2023-09-14 21:57:50 +0530

    Map playlist tracks

---
+++ src/components/track-bar/track-bar.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './track-bar.style.scss';
+
+function TrackBar({ track, index }) {
+    const date = new Date(track.added_at);
+
+    const comparisonDate = new Date("2010-01-01");
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    const formattedDate = date < comparisonDate ? '' : date.toLocaleDateString("en-US", options);
+
+    const minutes = Math.floor(track.track.duration_ms / 60000);
+    const seconds = Math.floor((track.track.duration_ms % 60000) / 1000);
+
+    const formattedTime = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+
+    return (
+        <div className='track-bar-container'>
+            <h1 className="count">{index}</h1>
+
+            <h1 className="track-info">
+                <div className="track-image">
+                    <img src="" alt="" />
+                </div>
+
+                <div className="track-details">
+                    <h1>{track.track.name}</h1>
+                    <p>{track.track.artists[0].name}</p>
+                </div>
+            </h1>
+
+            <h1 className="album-title">{track.track.album.name}</h1>
+
+            <h1 className="date">{formattedDate}</h1>
+
+            <div className="duration">{formattedTime}</div>
+        </div>
+    )
+}
+
+export default TrackBar;
--- src/pages/playlist-page/playlist-page.jsx
+++ src/pages/playlist-page/playlist-page.jsx
@@ -2,7 +2,9 @@ import React, { useContext } from 'react';
 import './playlist-page.style.scss';
 import { useParams } from 'react-router-dom';
 import { UserPlaylistsContext } from '../../context/user-playlists-context';
+import { PlaylistItemsContext } from '../../context/playlist-items-context';
 import MainNavbar from '../../components/main-navbar/main-navbar.component';
+import TrackBar from '../../components/track-bar/track-bar.component';
 
 function PlaylistPage() {
     const { playlistID } = useParams();
@@ -10,6 +12,14 @@ function PlaylistPage() {
     const { userPlaylists } = useContext(UserPlaylistsContext);
 
     const playlist = userPlaylists.filter(playlist => playlist.id === playlistID)[0];
+
+    const { playlistItems } = useContext(PlaylistItemsContext);
+
+    const playlistObject = playlistItems.filter(item => item.id === playlist.id)[0];
+
+    const tracks = playlistObject[playlist.name];
+
+    console.log(tracks);
 
     return (
         <div className='playlist-page-container'>
@@ -28,15 +38,39 @@ function PlaylistPage() {
                     <p className="playlist-description">{playlist.description}</p>
 
                     <div className="playlist-by">
-                        <span className="user">{playlist.owner.display_name} </span>
+                        <span className="user">{playlist.owner.display_name}</span>
 
                         <span className="total-songs"> • {playlist.tracks.total} songs</span>
                     </div>
                 </div>
             </div>
 
-            <div className="track-list">
-                Playlist songs
+            <div className="track-list-container">
+                <div className="track-list-header">Hello</div>
+
+                <div className="track-list-table-header">
+                    <h1 className="count-hash">#</h1>
+
+                    <h1 className="title">Title</h1>
+
+                    <h1 className="album-title">Album</h1>
+
+                    <h1 className="date">Date Added</h1>
+
+                    <div className="time-icon">i</div>
+                </div>
+
+                <div className="track-list">
+                    {tracks.map((track, index) => {
+                        return (
+                            <TrackBar 
+                                key={track.id} 
+                                track={track} 
+                                index={index+1} 
+                            />
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
